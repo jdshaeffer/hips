58798931139f746432bc78e4b38cc3c20a3471f8
J.D. Shaeffer <shaefferjd@gmail.com>
2023-07-15 13:22:12 -0400

    feat: player1 border detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import './App.css';
@@ -14,6 +14,11 @@ function Game() {
   const [direction, setDirection] = useState('');
   const [playerCount, setPlayerCount] = useState(0);
   const [playerId, setPlayerId] = useState(0);
+  const [collision1, setCollision1] = useState(false);
+
+  const player1Ref = useRef<HTMLDivElement>(null);
+  const borderRef = useRef<HTMLDivElement>(null);
+
   const validKeys = new Set([
     'ArrowUp',
     'ArrowDown',
@@ -27,9 +32,8 @@ function Game() {
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (!moving) {
-      if (validKeys.has(e.key)) {
+      if (validKeys.has(e.key) && !collision1) {
         setMoving(true);
-        console.log('Key pressed: ' + e.key);
         if (e.key === 'ArrowUp' || e.key === 'w') {
           setDirection('n');
         } else if (e.key === 'ArrowDown' || e.key === 's') {
@@ -54,9 +58,6 @@ function Game() {
           direction === 'sw'
         )
       ) {
-        console.log(
-          'Already moving: ' + direction + '; new key pressed: ' + e.key
-        );
         if (direction === 'n') {
           if (e.key === 'ArrowRight' || e.key === 'd') {
             setDirection('ne');
@@ -128,8 +129,40 @@ function Game() {
         setMoving(false);
         setDirection('');
       }
-      console.log('Key released: ' + e.key);
     }
+  };
+
+  const checkBorderCollision = () => {
+    const borderBuffer = 4;
+    const player1Div = player1Ref.current;
+    const borderDiv = borderRef.current;
+
+    const player1HitBox = player1Div!.getBoundingClientRect();
+    const borderHitBox = borderDiv!.getBoundingClientRect();
+
+    let borderDetected = new Set<string>();
+    if (player1HitBox.left <= borderHitBox.left + borderBuffer) {
+      borderDetected.add('w');
+    } else {
+      borderDetected.delete('w');
+    }
+    if (player1HitBox.right >= borderHitBox.right - borderBuffer) {
+      borderDetected.add('e');
+    } else {
+      borderDetected.delete('e');
+    }
+    if (player1HitBox.bottom >= borderHitBox.bottom - borderBuffer) {
+      borderDetected.add('s');
+    } else {
+      borderDetected.delete('s');
+    }
+    if (player1HitBox.top <= borderHitBox.top + borderBuffer) {
+      borderDetected.add('n');
+    } else {
+      borderDetected.delete('n');
+    }
+    console.log(borderDetected);
+    return borderDetected;
   };
 
   const move = () => {
@@ -137,65 +170,84 @@ function Game() {
     let dy = 0;
     const speed = 2;
     const diagspd = 1.4;
-    if (direction === 'n') {
+
+    const borderCollision = checkBorderCollision();
+
+    if (direction === 'n' && !borderCollision.has('n')) {
       dy -= speed;
-    } else if (direction === 's') {
+    } else if (direction === 's' && !borderCollision.has('s')) {
       dy += speed;
-    } else if (direction === 'w') {
+    } else if (direction === 'w' && !borderCollision.has('w')) {
       dx -= speed;
-    } else if (direction === 'e') {
+    } else if (direction === 'e' && !borderCollision.has('e')) {
       dx += speed;
-    } else if (direction === 'ne') {
+    } else if (
+      direction === 'ne' &&
+      !borderCollision.has('n') &&
+      !borderCollision.has('e')
+    ) {
       dy -= diagspd;
       dx += diagspd;
-    } else if (direction === 'nw') {
+    } else if (
+      direction === 'nw' &&
+      !borderCollision.has('n') &&
+      !borderCollision.has('w')
+    ) {
       dy -= diagspd;
       dx -= diagspd;
-    } else if (direction === 'se') {
+    } else if (
+      direction === 'se' &&
+      !borderCollision.has('s') &&
+      !borderCollision.has('e')
+    ) {
       dy += diagspd;
       dx += diagspd;
-    } else if (direction === 'sw') {
+    } else if (
+      direction === 'sw' &&
+      !borderCollision.has('s') &&
+      !borderCollision.has('w')
+    ) {
       dy += diagspd;
       dx -= diagspd;
     }
 
-    if (playerId === 1) {
-      setPosX1(posX1 + dx);
-      setPosY1(posY1 + dy);
-      socket.emit('updatePlayer1', { x: posX1, y: posY1 });
-    } else {
-      setPosX2(posX2 + dx);
-      setPosY2(posY2 + dy);
-      socket.emit('updatePlayer2', { x: posX2, y: posY2 });
-    }
+    // if (playerId === 1) {
+    setPosX1(posX1 + dx);
+    setPosY1(posY1 + dy);
+    //   socket.emit('updatePlayer1', { x: posX1, y: posY1 });
+    // } else {
+    //   setPosX2(posX2 + dx);
+    //   setPosY2(posY2 + dy);
+    //   socket.emit('updatePlayer2', { x: posX2, y: posY2 });
+    // }
   };
 
   // socket listener
-  useEffect(() => {
-    socket =
-      process.env.NODE_ENV === 'production'
-        ? io('https://nycmud.com', {
-            path: '/socket.io',
-          })
-        : io('http://localhost:5000');
-    socket.on('playerCountUpdate', (count: number) => {
-      setPlayerCount(count);
-    });
-    socket.on('assignPlayerId', (id: number) => {
-      setPlayerId(id);
-    });
-    socket.on('updatePlayer1', (pos) => {
-      setPosX1(pos.x);
-      setPosY1(pos.y);
-    });
-    socket.on('updatePlayer2', (pos) => {
-      setPosX2(pos.x);
-      setPosY2(pos.y);
-    });
-    socket.on('display', (res: string) => {
-      console.log(res);
-    });
-  }, []);
+  // useEffect(() => {
+  //   socket =
+  //     process.env.NODE_ENV === 'production'
+  //       ? io('https://nycmud.com', {
+  //           path: '/socket.io',
+  //         })
+  //       : io('http://localhost:5000');
+  //   socket.on('playerCountUpdate', (count: number) => {
+  //     setPlayerCount(count);
+  //   });
+  //   socket.on('assignPlayerId', (id: number) => {
+  //     setPlayerId(id);
+  //   });
+  //   socket.on('updatePlayer1', (pos) => {
+  //     setPosX1(pos.x);
+  //     setPosY1(pos.y);
+  //   });
+  //   socket.on('updatePlayer2', (pos) => {
+  //     setPosX2(pos.x);
+  //     setPosY2(pos.y);
+  //   });
+  //   socket.on('display', (res: string) => {
+  //     console.log(res);
+  //   });
+  // }, []);
 
   // move controller
   useEffect(() => {
@@ -220,29 +272,29 @@ function Game() {
     );
   } else {
     return (
-      <div className='center-box'>
-        <h1
+      <div ref={borderRef} className='center-box'>
+        <div
           id='player1'
+          ref={player1Ref}
           className='player'
           style={{
             top: posY1,
             left: posX1,
+            padding: '10px',
+            border: '2px solid white',
           }}
-        >
-          @
-        </h1>
+        />
         {playerCount > 1 && (
-          <h1
+          <div
             id='player2'
             className='player'
             style={{
               top: posY2,
               left: posX2,
-              color: 'red',
+              padding: '10px',
+              border: '2px solid red',
             }}
-          >
-            @
-          </h1>
+          />
         )}
       </div>
     );
