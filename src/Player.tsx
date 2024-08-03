0d7c1ec4628024d5f700c5398129071441f1bd90
benduncan <frankbaconman@gmail.com>
2024-08-02 22:04:12 -0600

    Need to fix the collider stuff. The algorithm was relient upon there being a p1 & p2 only. Now we need to iterate through all current players.

--- src/Player.tsx
+++ src/Player.tsx
@@ -87,91 +87,94 @@ function Player({ borderRef, socket }: Props) {
     }
   };
 
-   const getPlayerRefInfo = () => {
-     if (playerRef) {
-       const playerDiv = playerRef.current;
-       return playerDiv!.getBoundingClientRect();
-     }
-   };
+  const getPlayerRefInfo = () => {
+    if (playerRef) {
+      const playerDiv = playerRef.current;
+      return playerDiv!.getBoundingClientRect();
+    }
+  };
 
-   const getOpponentRefInfo = () => {
-     // optional since there may just be p1 running around, no p2 present
-     const opponentDiv = playerRef.current;
-     const opponentHitBox = opponentDiv
-       ? opponentDiv.getBoundingClientRect()
-       : null;
-     return opponentHitBox;
-   };
+  const getOpponentRefInfo = () => {
+    // optional since there may just be p1 running around, no p2 present
+    const opponentDiv = playerRef.current;
+    const opponentHitBox = opponentDiv
+      ? opponentDiv.getBoundingClientRect()
+      : null;
+    return opponentHitBox;
+  };
 
-   const checkPunchCollision = () => {
-     const punchHitBox = punchDiv?.getBoundingClientRect();
-     if (punchHitBox) {
-       const {
-         bottom: pBottom,
-         top: pTop,
-         left: pLeft,
-         right: pRight,
-       } = punchHitBox;
-       if (pBottom && pTop && pLeft && pRight) {
-         const opponentHitBox = getOpponentRefInfo();
-         if (opponentHitBox) {
-           const {
-             bottom: oBottom,
-             top: oTop,
-             left: oLeft,
-             right: oRight,
-           } = opponentHitBox;
-           if (
-             (pLeft >= oLeft || pRight >= oLeft) &&
-             (pLeft <= oRight || pRight <= oRight) &&
-             (pBottom <= oBottom || pTop <= oBottom) &&
-             (pBottom >= oTop || pTop >= oTop)
-           ) {
-             console.log('punch hit!');
-           }
-         }
-       }
-     }
-   };
+  const checkPunchCollision = () => {
+    console.log("a");
+    const punchHitBox = punchDiv?.getBoundingClientRect();
+    if (punchHitBox) {
+      console.log("b");
+      const {
+        bottom: pBottom,
+        top: pTop,
+        left: pLeft,
+        right: pRight,
+      } = punchHitBox;
+      if (pBottom && pTop && pLeft && pRight) {
+        console.log("c");
+        const opponentHitBox = getOpponentRefInfo();
+        if (opponentHitBox) {
+          console.log("d");
+          const {
+            bottom: oBottom,
+            top: oTop,
+            left: oLeft,
+            right: oRight,
+          } = opponentHitBox;
+          if (
+            (pLeft >= oLeft || pRight >= oLeft) &&
+            (pLeft <= oRight || pRight <= oRight) &&
+            (pBottom <= oBottom || pTop <= oBottom) &&
+            (pBottom >= oTop || pTop >= oTop)
+          ) {
+            console.log('punch hit!');
+          }
+        }
+      }
+    }
+  };
 
+  const checkBorderCollision = () => {
+    const borderBuffer = 4;
+    const playerHitBox = getPlayerRefInfo();
+    const borderDiv = borderRef.current;
+    const borderHitBox = borderDiv!.getBoundingClientRect();
 
-   const checkBorderCollision = () => {
-     const borderBuffer = 4;
-     const playerHitBox = getPlayerRefInfo();
-     const borderDiv = borderRef.current;
-     const borderHitBox = borderDiv!.getBoundingClientRect();
+    let borderDetected = new Set<string>();
+    if (playerHitBox) {
+      const checkCollision = (side: keyof DOMRect) =>
+        +playerHitBox[side] <= +borderHitBox[side] + borderBuffer;
 
-     let borderDetected = new Set<string>();
-     if (playerHitBox) {
-       const checkCollision = (side: keyof DOMRect) =>
-         +playerHitBox[side] <= +borderHitBox[side] + borderBuffer;
+      const checkCollisionAlt = (side: keyof DOMRect) =>
+        +playerHitBox[side] >= +borderHitBox[side] - borderBuffer;
 
-       const checkCollisionAlt = (side: keyof DOMRect) =>
-         +playerHitBox[side] >= +borderHitBox[side] - borderBuffer;
-
-       if (checkCollision('left')) {
-         borderDetected.add('w');
-       } else {
-         borderDetected.delete('w');
-       }
-       if (checkCollisionAlt('right')) {
-         borderDetected.add('e');
-       } else {
-         borderDetected.delete('e');
-       }
-       if (checkCollisionAlt('bottom')) {
-         borderDetected.add('s');
-       } else {
-         borderDetected.delete('s');
-       }
-       if (checkCollision('top')) {
-         borderDetected.add('n');
-       } else {
-         borderDetected.delete('n');
-       }
-     }
-     return borderDetected;
-   };
+      if (checkCollision('left')) {
+        borderDetected.add('w');
+      } else {
+        borderDetected.delete('w');
+      }
+      if (checkCollisionAlt('right')) {
+        borderDetected.add('e');
+      } else {
+        borderDetected.delete('e');
+      }
+      if (checkCollisionAlt('bottom')) {
+        borderDetected.add('s');
+      } else {
+        borderDetected.delete('s');
+      }
+      if (checkCollision('top')) {
+        borderDetected.add('n');
+      } else {
+        borderDetected.delete('n');
+      }
+    }
+    return borderDetected;
+  };
 
   const move = () => {
     if (borderRef === null) return;
@@ -201,11 +204,11 @@ function Player({ borderRef, socket }: Props) {
   };
 
   const emitPunchingUpdate = () => {
-			socket?.emit(`punchUpdate${socket.id}`, punching);
+    socket?.emit(`punchUpdate${socket.id}`, punching);
   };
 
   const emitPositionUpdate = () => {
-      socket?.emit(`positionUpdate${socket.id}`, { x, y, dir: direction });
+    socket?.emit(`positionUpdate${socket.id}`, { x, y, dir: direction });
   };
 
   const updatePlayerPosition = (dx: number, dy: number) => {
@@ -249,7 +252,7 @@ function Player({ borderRef, socket }: Props) {
 
   useEffect(() => {
     if (moving) requestAnimationFrame(move);
-		if (punching) {
+    if (punching) {
       const punchDiv = punchRef.current;
       if (punchDiv) {
         setPunchDiv(punchDiv);
@@ -262,7 +265,7 @@ function Player({ borderRef, socket }: Props) {
     <>
       <Sprite
         // id='player'
-				punchRef={punchRef}
+        punchRef={punchRef}
         ref={playerRef}
         x={x}
         y={y}
